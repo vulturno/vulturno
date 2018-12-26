9fc5171adcf1dc39994ce4d9c6130f4604a6ad38
Jorge Aznar <jorgeatgu@gmail.com>
2018-12-26 13:26:37 +0100

    estructura creada

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # vulturno
 La evolución de las temperaturas máximas en España
+
+
+
+| Ciudad        | Número        | Año     |
+| ------------- |:-------------:| -------:|
+| A coruña      | 1387E         |  1972   |
+| Valencia      | 8414A         |  1966   |
+| Sevilla       | 5783          |  1951   |
+| Malaga        | 6155A         |  1950   |
+| Murcia        | 7031          |  1950   |
+| Palma         | B278          |  1951   |
+| Gran Canaria  | C649I         |  1951   |
+| Bilbao        | 1082          |  1950   |
+| Alicante      | 8019          |  1967   |
+| Cordoba       | 5402          |  1960   |
+| Valladolid    | 2539          |  1950   |
+| Vitoria       | 9091R         |  2012   |
+| Granada       | 5530E         |  1973   |
+| Oviedo        | 1249X         |  2013   |
+| Tenerife      | C447A         |  1950   |
+| Pamplona      | 9263D         |  1975   |
+| Donostia      | 1014A         |  2012   |
+| Burgos        | 2331          |  1950   |
+| Albacete      | 8175          |  1950   |
+| Santander     | 1109          |  1954   |
+| Castellón     | 8501          |  1950   |
+| Logroño       | 9170          |  1950   |
+| Badajoz       | 4452          |  1955   |
+| Huelva        | 4605          |  1950   |
+| Salamanca     | 2867          |  1950   |
+| Lleida        | 9771          |  1960   |
+| Reus          | 0016A         |  1950   |
+| León          | 2661B         |  2014   |
+| Jerez         | 5960          |  1950   |
+| Jaen          | 5270          |  1950   |
+| Ourense       | 1690B         |  1952   |
+| Girona        | 0367          |  1973   |
+| Lugo          | 1505          |  1951   |
+| Santiago      | 1428          |  1950   |
+| Caceres       | 3469          |  1950   |
+| Melilla       | 6000A         |  1971   |
+| Ceuta         | 5000C         |  2009   |
+| Guadalajara   | 3168A         |  1950   |
+| Toledo        | 3260B         |  1982   |
+| Pontevedra    | 1484          |  1964   |
+| Palencia      | 2401          |  1950   |
+| Ciudad Real   | 4121C         |  1950   |
+| Zamora        | 2614          |  1956   |
+| Merida        | 4410X         |  1990   |
+| Avila         | 2444C         |  1953   |
+| Cuenca        | 8096          |  1951   |
+| Huesca        | 9898          |  1950   |
+| Segovia       | 2465A         |  1950   |
+| Soria         | 2030          |  1950   |
+| Teruel        | 8368U         |  1987   |
+| Almería       | 6325O         |  1968   |
---
+++ gulpfile.js
@@ -0,0 +1,179 @@
+var gulp = require('gulp');
+postcss = require('gulp-postcss');
+autoprefixer = require('gulp-autoprefixer');
+sourcemaps = require('gulp-sourcemaps');
+atImport = require('postcss-import');
+selector = require('postcss-custom-selectors')
+customProperties = require("postcss-custom-properties")
+sorting = require('postcss-sorting');
+nested = require('postcss-nested');
+reporter = require('postcss-reporter');
+imagemin = require('gulp-imagemin');
+newer = require('gulp-newer');
+nano = require('gulp-cssnano');
+notify = require('gulp-notify');
+stylelint = require('stylelint');
+browserSync = require('browser-sync');
+terser = require('gulp-terser');
+postcssNormalize = require('postcss-normalize');
+
+
+var paths = {
+  js: 'src/js',
+  css: 'src/css',
+  images: 'src/img/*',
+  buildCss: 'css/',
+  buildJs: 'js/',
+  buildImages: 'img/'
+};
+
+var watch = {
+  js: [
+    paths.js + '/**/*.js'
+  ],
+  css: [
+    paths.css + '/**/*.css'
+  ],
+  minifycss: [
+    paths.buildCss + '/**/*.css'
+  ],
+  images: [
+    paths.images + '/**/*.*'
+  ],
+  html: [
+  '/*.html'
+  ]
+};
+
+gulp.task("browserSync", function() {
+    browserSync({
+        server: {
+            baseDir: "./",
+            reloadDelay: 200
+        },
+        online: true
+    })
+});
+
+/* Notificando errores de JavaScript */
+function errorAlertJS(error) {
+    notify.onError({
+        title: "Gulp JavaScript",
+        subtitle: "Algo esta mal en tu JavaScript!",
+        sound: "Basso"
+    })(error);
+    console.log(error.toString());
+    this.emit("end");
+};
+
+/* Notificando errores de CSS */
+function errorAlertPost(error) {
+    notify.onError({
+        title: "Gulp postCSS",
+        subtitle: "Algo esta mal en tu CSS!",
+        sound: "Basso"
+    })(error);
+    console.log(error.toString());
+    this.emit("end");
+};
+
+
+/* ==========================================================================
+   Lanzando postCSS
+   ========================================================================== */
+
+/*
+ * El orden de los plugins debe ser respetado.
+ *
+ * Antes de que nuestro CSS empiece a ser transformado por los diferentes
+ * plugins vamos a 'lintear' nuestro CSS para seguir un orden y concierto.
+ *
+ *
+ */
+
+gulp.task('css', function() {
+    var processors = [
+
+        atImport({
+            plugins: [stylelint]
+        }),
+        stylelint,
+        reporter({
+            clearMessages: true
+        }),
+        nested,
+        customProperties,
+        selector,
+        sorting({
+            "sort-order": "csscomb"
+        }),
+        autoprefixer,
+        postcssNormalize({
+            browsers: 'last 2 versions',
+            forceImport: true
+        }),
+    ];
+    return gulp.src('./src/css/styles.css')
+
+    .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .on("error", errorAlertPost)
+        .pipe(sourcemaps.write('./', {
+            sourceRoot: '/src'
+        }))
+        .pipe(gulp.dest(paths.buildCss))
+        .pipe(browserSync.stream())
+        .pipe(notify({
+            message: 'postCSS complete'
+        }));
+});
+
+/* Lanzando CSSnano para comprimir CSS */
+gulp.task('minify', function() {
+    return gulp.src(watch.minifycss)
+    //Remove comments false //Z index
+        .pipe(nano())
+        .pipe(gulp.dest(paths.buildCss))
+        .pipe(notify({
+            message: 'CSSnano task complete'
+        }));
+});
+
+/* Comprimiendo imagenes */
+gulp.task('imagemin', function() {
+    return gulp.src(paths.images)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(paths.buildImages));
+});
+
+gulp.task('images', function() {
+    return gulp.src(paths.images)
+        .pipe(newer(paths.images))
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.buildImages));
+});
+
+/* Comprimiendo JavaScript */
+gulp.task('compress', function() {
+    return gulp.src(paths.js)
+        .pipe(newer(paths.js))
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.buildJs));
+});
+
+/* Tarea por defecto para compilar CSS y comprimir imagenes */
+gulp.task('default', ["browserSync"], function() {
+    //Add interval to watcher!
+    gulp.watch(watch.css, { interval: 300 }, ['css']);
+    gulp.watch(watch.images, { interval: 300 }, ['images']);
+    gulp.watch(["*.html", "css/*.css", "js/*.js", "./*.csv", "./*.json"]).on("change", browserSync.reload);
+});
+
+// Build para un proyecto sin imágenes
+gulp.task('build', ['minify', 'compress']);
