54062eb0f81cf3250f7db2ef08c2e2b20154f12c
Jorge Aznar <jorgeatgu@gmail.com>
2018-12-28 18:05:48 +0100

    clean data | remove null

--- json/vulturno.sh
+++ json/vulturno.sh
@@ -4,13 +4,14 @@
 Vamos a quedarnos solamente con el resumen anual de cada año.
 Y de ese resumen solamente con la temperatura media del año.
 Recorremos todas las estaciones con un for sobre el array de station.
-Con jq  creamos un JSON solamente con la fecha y la temperatura.
+Con jq creamos un JSON solamente con la fecha y la temperatura.
 Con sed eliminamos el -13 de la fecha.
-Y por último convertimos el json a csv
+Convertimos el json a csv.
+Y por último eliminamos todos los archivos que hemos creado con el nombre limpio.
 comentario
 
 # Array con todos los indicativos de todas las estaciones de la AEMET
-station=('5000C' '1249X' '4410X' '0370B' '4121C' '8501' '9771' '9381' '2444C' '3168A' '5270' '6297' '3469' '2401' '2465A' '3259' '9087' '5000A' '4605' '8368U' '1505' '9262' '3260B' '9263D' '0367' '8096' '5530E' '1249I' '6000A' '6325O' '8019' '5402' 'B278' '1109' '5960' '9898' '2614' '8175' '5514' 'C649I' '0016A' 'C447A' '2661' '4452' '2030' '2539' '9170' '6155A' '5783' '7031' '8416' '2331' '1428' '2867' '1387' '8025' '1082' '1024E')
+station=('5000C' '1249X' '4410X' '0370B' '4121C' '8501' '9771' '9381' '2444C' '3168A' '5270' '6297' '3469' '2401' '2465A' '3259' '9087' '5000A' '4605' '8368U' '1505' '2444' '9262' '3260B' '9263D' '0367' '8096' '5530E' '1249I' '6000A' '6325O' '8019' '5402' 'B278' '1109' '5960' '9898' '2614' '8175' '5514' 'C649I' '0016A' 'C447A' '2661' '4452' '2030' '2539' '9170' '6155A' '5783' '7031' '8416' '2331' '1428' '2867' '1387' '8025' '1082' '1024E')
 
 # Recorremos el array de numeros
 for (( i=0; i<${#station[@]}; ++i )); do
