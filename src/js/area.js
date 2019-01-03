255cef070f1b7fcd762ba6f32513786f98d4eacc
Jorge Aznar <jorgeatgu@gmail.com>
2019-01-03 15:27:27 +0100

    d3 annotation and rollup

--- src/js/area.js
+++ src/js/area.js
@@ -21,7 +21,6 @@ const vulturno = () => {
         .attr("class", "tooltip tooltip-temp")
         .style("opacity", 0);
     const bisectDate = d3.bisector(d => d.year).left;
-    console.log(bisectDate)
 
 
     const setupScales = () => {
@@ -230,8 +229,6 @@ const vulturno = () => {
                 d.yearmin = d.yearmin;
             });
 
-            console.log(datos)
-
             scales.count.x.range([0, width]);
             scales.count.y.range([height, 0]);
 
@@ -346,3 +343,371 @@ new SlimSelect({
   select: '#select-city',
   searchPlaceholder: 'Busca tu ciudad'
 })
+
+const maxvul = () => {
+    //Estructura similar a la que utilizan en algunos proyectos de pudding.cool
+    const margin = { top: 24, right: 48, bottom: 24, left: 24 };
+    let width = 0;
+    let height = 0;
+    let w = 0;
+    let h = 0;
+    const chart = d3.select('.chart-temperature-max');
+    const svg = chart.select('svg');
+    const scales = {};
+    let dataz;
+
+    //Escala para los ejes X e Y
+    const setupScales = () => {
+
+        const countX = d3.scaleLinear()
+            .domain(
+                [d3.min(dataz, function(d) {
+                        return d.fecha;
+                    }),
+                    d3.max(dataz, function(d) {
+                        return d.fecha;
+                    })
+                ]
+            );
+
+
+        const countY = d3.scaleLinear()
+            .domain([d3.min(dataz, d => d.total), d3.max(dataz, d => d.total)]);
+
+        scales.count = { x: countX, y: countY };
+
+    }
+
+    //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
+    const setupElements = () => {
+
+        const g = svg.select('.chart-temperature-max-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+
+        g.append('g').attr('class', 'chart-temperature-max-container-bis');
+
+    }
+
+    //Actualizando escalas
+    const updateScales = (width) => {
+        scales.count.x.range([0, width]);
+    }
+
+    //Dibujando ejes
+    const drawAxes = (g) => {
+
+        const axisX = d3.axisBottom(scales.count.x)
+            .tickFormat(d3.format("d"))
+            .ticks(10)
+            .tickPadding(30);
+
+        g.select(".axis-x")
+            .attr("transform", "translate(0," + (height / 2) + ")")
+            .call(axisX);
+
+    }
+
+    const updateChart = (dataz) => {
+        w = chart.node().offsetWidth;
+        h = 200;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg
+            .attr('width', w )
+            .attr('height', h);
+
+        const translate = "translate(" + margin.left + "," + margin.top + ")";
+
+        const g = svg.select('.chart-temperature-max-container')
+
+        g.attr("transform", translate)
+
+        updateScales(width, height)
+
+
+
+        const container = chart.select('.chart-temperature-max-container-bis')
+
+        const layer = container.selectAll('.circles-max')
+            .data(dataz)
+
+        const newLayer = layer.enter()
+            .append('circle')
+            .attr('class', 'circles-max')
+
+
+        layer.merge(newLayer)
+            .attr("cx", d => scales.count.x(d.fecha))
+            .attr("cy", (height / 2))
+            .attr("r", 0)
+            .transition()
+            .delay(function(d, i) {
+                return i * 10
+            })
+            .duration(300)
+            .attr("r", d => 3 * d.total)
+            .attr('fill-opacity', 0.6);
+
+        drawAxes(g)
+
+            //Add annotations
+            const labels = [{
+                    data: { year: 2012 },
+                    y: 100,
+                    dy: -50, dx: -52,
+                    note: {
+                        title: "Entre 2009 y 2017 se establecen el 78% de los records de máximas",
+                        wrap: 230,
+                        align: "middle"
+                    }
+                }
+            ].map(l => {
+                l.subject = { radius: 4 }
+                return l
+            })
+
+            window.makeAnnotations = d3.annotation()
+                .annotations(labels)
+                .type(d3.annotationCalloutCircle)
+                .accessors({
+                    x: d => scales.count.x(d.year),
+                    y: d => scales.count.y(d.total)
+                })
+                .accessorsInverse({
+                    year: d => scales.count.x.invert(d.x),
+                    total: d => scales.count.y.invert(d.y)
+                })
+                .on('subjectover', function(annotation) {
+                    annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+                        .classed("hidden", false)
+                })
+                .on('subjectout', function(annotation) {
+                    annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+                        .classed("hidden", true)
+                })
+
+            svg.append("g")
+                .attr("class", "annotation-test")
+                .call(makeAnnotations)
+
+            svg.selectAll("g.annotation-connector, g.annotation-note")
+
+    }
+
+    const resize = () => {
+        updateChart(dataz)
+    }
+
+    // LOAD THE DATA
+    const loadData = () => {
+
+        d3.csv('csv/max-record.csv', (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data
+                dataz.forEach(d => {
+                    d.fecha = d.yearmax;
+                    d.total = d.totalmax;
+                });
+                setupElements()
+                setupScales()
+                updateChart(dataz)
+            }
+
+        });
+    }
+
+    window.addEventListener('resize', resize)
+
+    loadData()
+
+}
+
+maxvul()
+
+const minvul = () => {
+    //Estructura similar a la que utilizan en algunos proyectos de pudding.cool
+    const margin = { top: 24, right: 48, bottom: 24, left: 24 };
+    let width = 0;
+    let height = 0;
+    let w = 0;
+    let h = 0;
+    const chart = d3.select('.chart-temperature-min');
+    const svg = chart.select('svg');
+    const scales = {};
+    let dataz;
+
+    //Escala para los ejes X e Y
+    const setupScales = () => {
+
+        const countX = d3.scaleLinear()
+            .domain(
+                [d3.min(dataz, function(d) {
+                        return d.fecha;
+                    }),
+                    d3.max(dataz, function(d) {
+                        return d.fecha;
+                    })
+                ]
+            );
+
+        scales.count = { x: countX };
+
+    }
+
+    //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
+    const setupElements = () => {
+
+        const g = svg.select('.chart-temperature-min-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+
+        g.append('g').attr('class', 'chart-temperature-min-container-bis');
+
+    }
+
+    //Actualizando escalas
+    const updateScales = (width) => {
+        scales.count.x.range([0, width]);
+    }
+
+    //Dibujando ejes
+    const drawAxes = (g) => {
+
+        const axisX = d3.axisBottom(scales.count.x)
+            .tickFormat(d3.format("d"))
+            .ticks(10)
+            .tickPadding(30);
+
+        g.select(".axis-x")
+            .attr("transform", "translate(0," + (height / 2) + ")")
+            .call(axisX);
+
+    }
+
+    const updateChart = (dataz) => {
+        w = chart.node().offsetWidth;
+        h = 200;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg
+            .attr('width', w )
+            .attr('height', h);
+
+        const translate = "translate(" + margin.left + "," + margin.top + ")";
+
+        const g = svg.select('.chart-temperature-min-container')
+
+        g.attr("transform", translate)
+
+        updateScales(width, height)
+
+        drawAxes(g)
+
+        const container = chart.select('.chart-temperature-min-container-bis')
+
+        const layer = container.selectAll('.circles-min')
+            .data(dataz)
+
+        const newLayer = layer.enter()
+            .append('circle')
+            .attr('class', 'circles-min')
+
+
+        layer.merge(newLayer)
+            .attr("cx", d => scales.count.x(d.fecha))
+            .attr("cy", (height / 2))
+            .attr("r", 0)
+            .transition()
+            .delay(function(d, i) {
+                return i * 10
+            })
+            .duration(300)
+            .attr("r", d => 3 * d.total)
+            .attr('fill-opacity', 0.6);
+
+        //Add annotations
+        const labels = [{
+                data: { year: 1988.5 },
+                y: 100,
+                dy: -50,
+                note: {
+                    title: "Desde 1986 no se bate ningún récord de temperatura mínima",
+                    wrap: 230,
+                    align: "middle"
+                }
+            }
+        ].map(l => {
+            l.subject = { radius: 4 }
+            return l
+        })
+
+        window.makeAnnotations = d3.annotation()
+            .annotations(labels)
+            .type(d3.annotationCalloutCircle)
+            .accessors({
+                x: d => scales.count.x(d.year),
+                y: d => scales.count.y(d.total)
+            })
+            .accessorsInverse({
+                year: d => scales.count.x.invert(d.x),
+                total: d => scales.count.y.invert(d.y)
+            })
+            .on('subjectover', function(annotation) {
+                annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+                    .classed("hidden", false)
+            })
+            .on('subjectout', function(annotation) {
+                annotation.type.a.selectAll("g.annotation-connector, g.annotation-note")
+                    .classed("hidden", true)
+            })
+
+        svg.append("g")
+            .attr("class", "annotation-test")
+            .call(makeAnnotations)
+
+        svg.selectAll("g.annotation-connector, g.annotation-note")
+
+
+    }
+
+    const resize = () => {
+        updateChart(dataz)
+    }
+
+    // LOAD THE DATA
+    const loadData = () => {
+
+        d3.csv('csv/min-record.csv', (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data
+                dataz.forEach(d => {
+                    d.fecha = d.yearmin;
+                    d.total = d.totalmin;
+                });
+                setupElements()
+                setupScales()
+                updateChart(dataz)
+            }
+
+        });
+    }
+
+    window.addEventListener('resize', resize)
+
+    loadData()
+
+}
+
+minvul()
+
