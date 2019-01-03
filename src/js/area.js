b83fb1a9dc717a082720efaeabfc873f20cce6a7
Jorge Aznar <jorgeatgu@gmail.com>
2019-01-03 21:18:58 +0100

    menu

--- src/js/area.js
+++ src/js/area.js
@@ -1,3 +1,37 @@
+function menu() {
+    var overlay = document.querySelector('.overlay');
+    var navigation = document.querySelector('.navegacion');
+    var body = document.querySelector('body');
+    var elementBtn = document.querySelectorAll('.navegacion-btn');
+    var burger = document.querySelector('.burger');
+
+    function classToggle() {
+        burger.classList.toggle('clicked');
+        overlay.classList.toggle('show');
+        navigation.classList.toggle('show');
+        body.classList.toggle('overflow');
+    }
+
+    document.querySelector('.burger').addEventListener('click', classToggle);
+    document.querySelector('.overlay').addEventListener('click', classToggle);
+
+    for(i=0; i<elementBtn.length; i++){
+        elementBtn[i].addEventListener("click", function(){
+            removeClass();
+            console.log('click')
+        });
+    }
+
+    function removeClass() {
+        overlay.classList.remove("show");
+        navigation.classList.remove("show");
+        burger.classList.remove("clicked");
+
+    }
+}
+
+menu();
+
 const vulturno = () => {
 
     const widthMobile = (window.innerWidth > 0) ? window.innerWidth : screen.width;
