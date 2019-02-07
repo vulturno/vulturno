26507ee592122eefcbf7a93314d8f35114bc0ded
Jorge Aznar <jorgeatgu@gmail.com>
2019-02-07 22:52:35 +0100

    remove tooltips :busstop:

--- src/js/area.js
+++ src/js/area.js
@@ -67,23 +67,6 @@ const vulturno = () => {
 
     }
 
-    const tooltips = (data) => {
-
-        tooltipMax = chart.append("div")
-            .attr("class", "tooltip tooltip-media-anual-maxima")
-            .attr("id", "tooltip-max");
-
-        tooltipMin = chart.append("div")
-            .attr("class", "tooltip tooltip-media-anual-minima");
-
-        tooltipMax.data(datos)
-            .html(function(d) { return "<p class='tooltip-media-texto'>La temperatura media máxima fue de <strong>" + d3.max(datos, d => d.tempmax ) + "ºC</strong> en <strong>" + d.yearmax; + "</strong></p>" });
-
-        tooltipMin.data(datos)
-            .html(function(d) { return "<p class='tooltip-media-texto'>La temperatura media mínima fue de <strong>" + d3.min(datos, d => d.tempmin ) + "ºC</strong> en <strong>" + d.yearmin; + "</strong></p>" });
-
-    }
-
     //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
     const setupElements = () => {
 
@@ -272,12 +255,6 @@ const vulturno = () => {
             scales.count = { x: countX, y: countY };
             updateChart(datos)
 
-            tooltipMax.data(datos)
-                .html(function(d) { return "<p class='tooltip-media-texto'>La temperatura media máxima fue de <strong>" + d3.max(datos, d => d.tempmax ) + "ºC</strong> en <strong>" + d.yearmax; + "</strong></p>" });
-
-            tooltipMin.data(datos)
-                .html(function(d) { return "<p class='tooltip-media-texto'>La temperatura media mínima fue de <strong>" + d3.min(datos, d => d.tempmin ) + "ºC</strong> en <strong>" + d.yearmin; + "</strong></p>" })
-
         });
 
 
@@ -368,7 +345,6 @@ const vulturno = () => {
                 setupElements()
                 setupScales()
                 updateChart(datos)
-                tooltips(datos)
                 mes = 'Albacete';
                 update(mes)
             }
@@ -796,7 +772,7 @@ const tropicalTotal = () => { //Estructura similar a la que utilizan en algunos 
             .domain([d3.min(dataz, d => d.year), d3.max(dataz, d => d.year)]);
 
         const countY = d3.scaleLinear()
-            .domain([0, d3.max(dataz, d => d.total)]);
+            .domain([0, d3.max(dataz, d => d.total * 1.25)]);
 
         scales.count = { x: countX, y: countY };
 
@@ -891,7 +867,141 @@ const tropicalTotal = () => { //Estructura similar a la que utilizan en algunos 
     // LOAD THE DATA
     const loadData = () => {
 
-        d3.csv('csv/total-dias-tropicales.csv', (error, data) => {
+        d3.csv('csv/total-dias-temp-30.csv', (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data
+                dataz.forEach(d => {
+                    d.year = d.year;
+                    d.total = +d.total;
+                });
+                setupElements()
+                setupScales()
+                updateChart(dataz)
+            }
+
+        });
+    }
+
+    window.addEventListener('resize', resize)
+
+    loadData()
+
+}
+
+const frostyTotal = () => { //Estructura similar a la que utilizan en algunos proyectos de pudding.cool
+    const margin = { top: 24, right: 24, bottom: 24, left: 32 };
+    let width = 0;
+    let height = 0;
+    const chart = d3.select('.chart-frosty');
+    const svg = chart.select('svg');
+    const scales = {};
+    let dataz;
+
+    //Escala para los ejes X e Y
+    const setupScales = () => {
+
+        const countX = d3.scaleTime()
+            .domain([d3.min(dataz, d => d.year), d3.max(dataz, d => d.year)]);
+
+        const countY = d3.scaleLinear()
+            .domain([0, d3.max(dataz, d => d.total)]);
+
+        scales.count = { x: countX, y: countY };
+
+    }
+
+    //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
+    const setupElements = () => {
+
+        const g = svg.select('.chart-frosty-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+        g.append('g').attr('class', 'axis axis-y');
+
+        g.append('g').attr('class', 'chart-frosty-container-bis');
+
+    }
+
+    //Actualizando escalas
+    const updateScales = (width, height) => {
+        scales.count.x.range([0, width]);
+        scales.count.y.range([height, 0]);
+    }
+
+    //Dibujando ejes
+    const drawAxes = (g) => {
+
+        const axisX = d3.axisBottom(scales.count.x)
+            .tickFormat(d3.format("d"))
+            .ticks(13)
+
+        g.select(".axis-x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(axisX)
+
+        const axisY = d3.axisLeft(scales.count.y)
+            .tickFormat(d3.format("d"))
+            .ticks(5)
+            .tickSizeInner(-width)
+
+        g.select(".axis-y")
+            .call(axisY)
+    }
+
+    const updateChart = (dataz) => {
+        const w = chart.node().offsetWidth;
+        const h = 600;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg
+            .attr('width', w)
+            .attr('height', h);
+
+        const translate = "translate(" + margin.left + "," + margin.top + ")";
+
+        const g = svg.select('.chart-frosty-container')
+
+        g.attr("transform", translate)
+
+        const area = d3.area()
+            .x(d => scales.count.x(d.year))
+            .y0(height)
+            .y1(d => scales.count.y(d.total))
+
+        updateScales(width, height)
+
+        const container = chart.select('.chart-frosty-container-bis')
+
+        const layer = container.selectAll('.area-frosty')
+            .data([dataz])
+
+        const newLayer = layer.enter()
+            .append('path')
+            .attr('class', 'area-frosty')
+
+        layer.merge(newLayer)
+            .transition()
+            .duration(600)
+            .ease(d3.easeLinear)
+            .attr('d', area)
+
+        drawAxes(g)
+
+    }
+
+    const resize = () => {
+        updateChart(dataz)
+    }
+
+    // LOAD THE DATA
+    const loadData = () => {
+
+        d3.csv('csv/total-dias-heladas.csv', (error, data) => {
             if (error) {
                 console.log(error);
             } else {
@@ -924,4 +1034,5 @@ new SlimSelect({
 })
 maxvul()
 tropicalTotal()
+frostyTotal()
 minvul()
