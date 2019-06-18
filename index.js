7d60090d9de81af17b0a803cb1248d2501913208
Jorge Aznar <jorgeatgu@gmail.com>
2019-06-18 16:16:43 +0200

    new chart mean temperature

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export {
 } from 'd3-array';
 
 export {
-    line, curveCardinal, arc, curveLinear, area,
+    line, curveCardinal, arc, curveLinear, area, curveStep
 } from 'd3-shape';
 
 export {
--- src/js/area.js
+++ src/js/area.js
@@ -2640,3 +2640,166 @@ tropicalTotal();
 frostyTotal();
 minvul();
 tempExt();
+
+const average = () => {
+    const margin = { top: 24, right: 24, bottom: 24, left: 40 };
+    let width = 0;
+    let height = 0;
+    const chart = d3.select('.line-average');
+    const svg = chart.select('svg');
+    const scales = {};
+    const temp = 'ºC';
+    let dataz;
+
+    const setupScales = () => {
+        const countX = d3.scaleBand().domain(dataz.map((d) => d.fecha));
+
+        const countY = d3
+            .scaleLinear()
+            .domain([
+                d3.min(dataz, (d) => d.mediaXX - 4),
+                d3.max(dataz, (d) => d.mediaXX + 4),
+            ]);
+
+        scales.count = { x: countX, y: countY };
+    };
+
+    const setupElements = () => {
+        const g = svg.select('.line-average-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+        g.append('g').attr('class', 'axis axis-y');
+
+        g.append('g').attr('class', 'line-average-container-dos');
+    };
+
+    const updateScales = (width, height) => {
+        scales.count.x.range([0, width]).paddingInner(0.2);
+        scales.count.y.range([height, 0]);
+    };
+
+    const drawAxes = (g) => {
+        const axisX = d3
+            .axisBottom(scales.count.x)
+            .tickFormat(d3.format('d'))
+            .ticks(13);
+
+        g.select('.axis-x')
+            .attr('transform', `translate(0,${height})`)
+            .call(axisX);
+
+        const axisY = d3
+            .axisLeft(scales.count.y)
+            .tickFormat((d) => d + temp)
+            .ticks(5)
+            .tickSizeInner(-width);
+
+        g.select('.axis-y').call(axisY);
+
+        g.append('text')
+            .attr('class', 'legend-aragon')
+            .attr('y', '10%')
+            .attr('x', '3%')
+            .text('Media de temperatura máxima entre 1980-2009');
+
+        g.append('rect')
+            .attr('class', 'legend-line')
+            .attr('y', '9%')
+            .attr('x', '1%')
+            .attr('height', '3px')
+            .attr('width', '16px');
+    };
+
+    const updateChart = (dataz) => {
+        const w = chart.node().offsetWidth;
+        const h = 600;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg.attr('width', w).attr('height', h);
+
+        const translate = `translate(${margin.left},${margin.top})`;
+
+        const g = svg.select('.line-average-container');
+
+        g.attr('transform', translate);
+
+        const line = d3
+            .line()
+            .x((d) => scales.count.x(d.fecha) + 16)
+            .y((d) => scales.count.y(d.mediaXX))
+            .curve(d3.curveStep);
+
+        updateScales(width, height);
+
+        const container = chart.select('.line-average-container-dos');
+
+        const layer = container.selectAll('.line').data([dataz]);
+
+        const layer2 = container.selectAll('.bar-vertical').data(dataz);
+
+        const newLayer2 = layer2
+            .enter()
+            .append('rect')
+            .attr('class', (d) => {
+                if (d.diff < 0) {
+                    return 'up';
+                } else {
+                    return 'down';
+                }
+            });
+
+        const newLayer = layer
+            .enter()
+            .append('path')
+            .attr('class', 'line')
+            .attr('stroke-width', '1.5');
+
+        layer2
+            .merge(newLayer2)
+            .attr('width', (d) => scales.count.x.bandwidth())
+            .attr('x', (d) => scales.count.x(d.fecha) + 3)
+            .attr('y', (d) => {
+                if (d.diff > 0) {
+                    return scales.count.y(d.mediaXX);
+                } else {
+                    return scales.count.y(d.mediaXX) - Math.abs(scales.count.y(d.diff) - scales.count.y(0));
+                }
+            })
+            .attr('height', (d) => Math.abs(scales.count.y(d.diff) - scales.count.y(0)));
+
+        layer.merge(newLayer).attr('d', line);
+
+        drawAxes(g);
+    };
+
+    const resize = () => {
+        updateChart(dataz);
+    };
+
+    const loadData = () => {
+        d3.csv('csv/julio-1980-2019.csv', (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data;
+                dataz.forEach((d) => {
+                    d.mediaXX = +d.mediaXX;
+                    d.mediaXXI = +d.mediaXXI;
+                    d.diff = +d.diff;
+                });
+                setupElements();
+                setupScales();
+                updateChart(dataz);
+            }
+        });
+    };
+
+    window.addEventListener('resize', resize);
+
+    loadData();
+};
+
+average();
