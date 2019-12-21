cd9ffae6839d60ebcd7cb93b34e9ebea33c895df
Jorge Aznar <jorgeatgu@gmail.com>
2019-12-21 13:36:31 +0100

    components for every fucntion

--- src/js/index.js
+++ src/js/index.js
@@ -1,106 +1,13 @@
-function menu() {
-    var overlay = document.querySelector('.overlay');
-    var navigation = document.querySelector('.navegacion');
-    var body = document.querySelector('body');
-    var elementBtn = document.querySelectorAll('.navegacion-btn');
-    var burger = document.querySelector('.burger');
-
-    function classToggle() {
-        burger.classList.toggle('clicked');
-        overlay.classList.toggle('show');
-        navigation.classList.toggle('show');
-        body.classList.toggle('overflow');
-    }
-
-    document.querySelector('.burger').addEventListener('click', classToggle);
-    document.querySelector('.overlay').addEventListener('click', classToggle);
-
-    for (let i = 0; i < elementBtn.length; i++) {
-        elementBtn[i].addEventListener('click', function() {
-            removeClass();
-        });
-    }
-
-    function removeClass() {
-        overlay.classList.remove('show');
-        navigation.classList.remove('show');
-        burger.classList.remove('clicked');
-    }
-}
+import menu from './components/menu.js';
+import quotes from './components/quotes.js';
+import quotesTemp from './components/quotesTemp.js';
 
 menu();
-
-function quotes() {
-    const list = [
-        '<span class="notas-text">La Central Térmica de <strong>AS Pontes</strong> propiedad de <strong>Endesa</strong> es el <strong>mayor emisor de CO2<strong> en España.',
-        '<span class="notas-text">La Central Térmica de <strong>Aboño</strong> propiedad de <strong>Endesa</strong> es el <strong>segundo mayor emisor de CO2<strong> en España.',
-        '<span class="notas-text">La Central Térmica de <strong>Litoral</strong> propiedad de <strong>Endesa</strong> es el <strong>tercer emisor de CO2<strong> en España.',
-        '<span class="notas-text">La Siderurgica de <strong>ArcelorMittal</strong> es el <strong>cuarto emisor de CO2<strong> en España.',
-        '<span class="notas-text">La Central Térmica de <strong>Teruel</strong> propiedad de <strong>Endesa</strong> es el <strong>quinto emisor de CO2<strong> en España.'
-    ];
-
-    const randomQuote = Math.floor(Math.random() * list.length);
-    document.getElementById('notas').innerHTML = list[randomQuote];
-}
-
 quotes();
 
-function quotesTemp() {
-    const list = [
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Zaragoza</strong>.</span><span class="notas-text-number">44.5ºC</span><span class="notas-text-date">07/07/2015</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Albacete</strong>.</span><span class="notas-text-number">42.6ºC</span><span class="notas-text-date">17/07/1978</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Alicante</strong>.</span><span class="notas-text-number">42.6ºC</span><span class="notas-text-date">04/07/1994</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Almería</strong>.</span><span class="notas-text-number">41.2ºC</span><span class="notas-text-date">30/07/1981</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Avila</strong>.</span><span class="notas-text-number">37.6ºC</span><span class="notas-text-date">24/07/1995</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Badajoz</strong>.</span><span class="notas-text-number">45.4ºC</span><span class="notas-text-date">13/07/2017</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Barcelona</strong>.</span><span class="notas-text-number">37.4ºC</span><span class="notas-text-date">28/08/2010</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Bilbao</strong>.</span><span class="notas-text-number">41.9ºC</span><span class="notas-text-date">04/08/2003</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Burgos</strong>.</span><span class="notas-text-number">38.8ºC</span><span class="notas-text-date">04/08/2003</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Caceres</strong>.</span><span class="notas-text-number">43.2ºC</span><span class="notas-text-date">13/07/2017</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Castellón</strong>.</span><span class="notas-text-number">40.6ºC</span><span class="notas-text-date">23/07/2009</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Ciudad Real</strong>.</span><span class="notas-text-number">43.7ºC</span><span class="notas-text-date">13/07/2009</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Cordoba</strong>.</span><span class="notas-text-number">46.9ºC</span><span class="notas-text-date">13/07/2009</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>A Coruña</strong>.</span><span class="notas-text-number">39.6ºC</span><span class="notas-text-date">28/08/1961</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Donostia</strong>.</span><span class="notas-text-number">38.6ºC</span><span class="notas-text-date">04/08/2003</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Girona</strong>.</span><span class="notas-text-number">43ºC</span><span class="notas-text-date">28/06/2019</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Gran Canaria</strong>.</span><span class="notas-text-number">44.2ºC</span><span class="notas-text-date">13/07/1952</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Granada</strong>.</span><span class="notas-text-number">43.5ºC</span><span class="notas-text-date">13/07/2017</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Huelva</strong>.</span><span class="notas-text-number">43.8ºC</span><span class="notas-text-date">25/07/2004</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Huesca</strong>.</span><span class="notas-text-number">42.6ºC</span><span class="notas-text-date">07/07/1982</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Jerez</strong>.</span><span class="notas-text-number">45.1ºC</span><span class="notas-text-date">07/08/2003</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>León</strong>.</span><span class="notas-text-number">38.2ºC</span><span class="notas-text-date">13/08/1987</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Lleida</strong>.</span><span class="notas-text-number">43.4ºC</span><span class="notas-text-date">29/06/2019</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Logroño</strong>.</span><span class="notas-text-number">42.8ºC</span><span class="notas-text-date">07/07/1982</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Madrid</strong>.</span><span class="notas-text-number">40.7ºC</span><span class="notas-text-date">28/06/2019</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Málaga</strong>.</span><span class="notas-text-number">44.2ºC</span><span class="notas-text-date">18/07/2019</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Mallorca</strong>.</span><span class="notas-text-number">41.4ºC</span><span class="notas-text-date">25/06/2001</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Melilla</strong>.</span><span class="notas-text-number">41.8ºC</span><span class="notas-text-date">06/07/1994</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Murcia</strong>.</span><span class="notas-text-number">40.5ºC</span><span class="notas-text-date">12/07/1961</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Ourense</strong>.</span><span class="notas-text-number">42.6ºC</span><span class="notas-text-date">20/07/1990</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Oviedo</strong>.</span><span class="notas-text-number">37ºC</span><span class="notas-text-date">18/07/2016</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Palencia</strong>.</span><span class="notas-text-number">40ºC</span><span class="notas-text-date">28/06/2019</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Pamplona</strong>.</span><span class="notas-text-number">40ºC</span><span class="notas-text-date">18/08/2012</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Pontevedra</strong>.</span><span class="notas-text-number">40ºC</span><span class="notas-text-date">14/06/2012</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Reus</strong>.</span><span class="notas-text-number">39.8ºC</span><span class="notas-text-date">04/08/2012</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Salamanca</strong>.</span><span class="notas-text-number">41ºC</span><span class="notas-text-date">10/08/2012</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Santander</strong>.</span><span class="notas-text-number">37.6ºC</span><span class="notas-text-date">30/06/1968</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Santiago</strong>.</span><span class="notas-text-number">39.4ºC</span><span class="notas-text-date">20/07/1990</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Segovia</strong>.</span><span class="notas-text-number">39.2ºC</span><span class="notas-text-date">15/07/1967</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Sevilla</strong>.</span><span class="notas-text-number">46.6ºC</span><span class="notas-text-date">23/07/1995</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Soria</strong>.</span><span class="notas-text-number">38ºC</span><span class="notas-text-date">28/07/1951</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Tenerife</strong>.</span><span class="notas-text-number">41.4ºC</span><span class="notas-text-date">31/07/2009</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Toledo</strong>.</span><span class="notas-text-number">43.1ºC</span><span class="notas-text-date">10/08/2012</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Valencia</strong>.</span><span class="notas-text-number">43ºC</span><span class="notas-text-date">27/08/2010</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Valladolid</strong>.</span><span class="notas-text-number">39.4ºC</span><span class="notas-text-date">24/07/1995</span>',
-        '<span class="notas-text-temp">Temperatura más alta registrada en <strong>Vitoria</strong>.</span><span class="notas-text-number">40.8ºC</span><span class="notas-text-date">10/08/2012</span>'
-    ];
-
-    const randomQuote = Math.floor(Math.random() * list.length);
-    document.getElementById('notas-temp').innerHTML = list[randomQuote];
-}
-quotesTemp();
-
-setInterval(() => { quotesTemp() }, 3000);
+setInterval(() => {
+    quotesTemp();
+}, 3000);
 
 const selectVulturno = document.getElementById('select-city');
 const selectScatter = document.getElementById('select-scatter-city');
@@ -255,7 +162,10 @@ function forceLayout(csvFile, record, color) {
                     .y(h * 0.5)
             )
             .force('charge', d3.forceManyBody().strength(5))
-            .force('collision', d3.forceCollide().radius((d) => d.radius + 1));
+            .force(
+                'collision',
+                d3.forceCollide().radius((d) => d.radius + 1)
+            );
 
         simulation
             .nodes(dataz)
@@ -2960,9 +2870,3 @@ new SlimSelect({
     select: '#select-cities-records-min',
     searchPlaceholder: 'Selecciona una ciudad'
 });
-
-/*new SlimSelect({
-    select: '#select-heat-wave',
-    searchPlaceholder: 'Selecciona una ciudad'
-});
-*/
